451dab829129d0545705f7b71489f0f6992b502a
santos saldaña <grinch.ssas@gmail.com>
2019-11-14 19:44:28 -0500

    clase animal

---
+++ anima.php
@@ -0,0 +1,22 @@
+<?php 
+class animal{
+    private $id;
+    private $nombre;
+    private $peso;
+             
+    public function getid(){
+    return $this->id;
+    }
+    public function getnombre(){
+    return $this->nombre;
+    }
+   
+   public function setpeso($peso){
+        $this->peso=$peso;
+    }
+
+    public function getpeso(){
+        return $this->peso;
+    }
+
+}               
---
+++ perro.php
@@ -0,0 +1,15 @@
+<?php
+require_once('animal.php');
+class gato extends animal{
+ private $tamaño_bigote;
+
+ public function __construct($id,$nombre,$persona,$tamaño_bigote)
+ {
+    parent:: __construct($id,$nombre,$persona);
+    $this->tamaño_bigote=$tamaño_bigote;
+ }
+ public function gettamaño_bigote()
+ {
+     return $this->tamaño_bigote;
+ }
+} 
