f3c9cba7689963a19dee65b7a2a7e9141c2a646a
santos saldaña <grinch.ssas@gmail.com>
2019-11-14 19:45:41 -0500

    clase perro

--- perro.php
+++ perro.php
@@ -3,7 +3,7 @@ require_once('animal.php');
 class gato extends animal{
  private $tamaño_bigote;
 
- public function __construct($id,$nombre,$persona,$tamaño_bigote)
+ public function __construct($id,$nombre,$tamaño_bigote)
  {
     parent:: __construct($id,$nombre,$persona);
     $this->tamaño_bigote=$tamaño_bigote;
